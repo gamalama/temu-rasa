c5a8f4fadede58fb38102f3fbac2808247928b8a
Fakhry Linux <fakhry.linux@gmail.com>
2024-11-18 01:49:32 +0800

    Offline Capability dengan Workbox

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,6 +1,5 @@
 const CONFIG = {
   BASE_URL: 'https://restaurant-api.dicoding.dev/',
-  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: {
     app: path.resolve(__dirname, 'src/scripts/index.js'),
-    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
   },
   output: {
     filename: '[name].bundle.js',
@@ -40,6 +40,25 @@ module.exports = {
         {
           from: path.resolve(__dirname, 'src/public/'),
           to: path.resolve(__dirname, 'dist/'),
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      swDest: './sw.bundle.js',
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurant-api',
+          },
+        },
+        {
+          urlPattern: ({ url }) => url.href.startsWith('https://restaurant-api.dicoding.dev/images/'),
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'restaurant-image-api',
+          },
         },
       ],
     }),
