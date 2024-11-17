0480605393d4a42d4d61711ffa4c3425b06eaedb
Fakhry Linux <fakhry.linux@gmail.com>
2024-11-17 22:04:37 +0800

    Menerapkan Cache pada Service Worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,25 @@
-self.addEventListener('install', (event) => {
-  console.log('Installing Service Worker ...');
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
 
-  // TODO: Caching App Shell Resource
+const assetsToCache = [
+  './',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './index.html',
+  './favicon.png',
+  './app.bundle.js',
+  './app.webmanifest',
+  './sw.bundle.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(CacheHelper.cachingAppShell([...assetsToCache]));
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Activating Service Worker ...');
-
-  // TODO: Delete old caches
+  event.waitUntil(CacheHelper.deleteOldCache());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log(event.request);
-
-  event.respondWith(fetch(event.request));
-  // TODO: Add/get fetch request to/from caches
+  event.respondWith(CacheHelper.revalidateCache(event.request));
 });
