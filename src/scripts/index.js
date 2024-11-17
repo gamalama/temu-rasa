6977a5dea4a6895a7f11eb011e33394f8019270e
Fakhry Linux <fakhry.linux@gmail.com>
2024-11-17 14:38:35 +0800

    Add eslint and formatting code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,7 +2,6 @@ import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/reset.css';
 import '../styles/main.css';
 import '../styles/responsive.css';
-import data from '../public/data/DATA.json'
 
 const menu = document.querySelector('#menu');
 const drawer = document.querySelector('#drawer');
