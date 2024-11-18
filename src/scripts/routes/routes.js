88ef9c7b0af76e8153d4bf815cb715b7ad75c204
Fakhry Linux <fakhry.linux@gmail.com>
2024-11-18 08:32:32 +0800

    Add Favorite feature

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,11 @@
 import Restaurants from '../views/pages/restaurants';
-import Favorite from '../views/pages/favorite';
 import Detail from '../views/pages/detail';
+import Favorite from '../views/pages/favorite';
 
 const routes = {
   '/': Restaurants,
-  '/favorite': Favorite,
   '/detail/:id': Detail,
+  '/favorite': Favorite,
 };
 
 export default routes;
--- src/scripts/views/pages/favorite.js
+++ src/scripts/views/pages/favorite.js
@@ -1,12 +1,26 @@
+import FavoriteRestaurantIdb from '../../data/favorite-restaurant-idb';
+import { createRestaurantItemTemplate } from '../templates/template-creator';
+
 const Favorite = {
-  async render(){
+  async render() {
     return `
-        <h2>Favorite</h2>
+        <form class="input-bar">
+          <label for="search-input"></label>
+          <input id="search-input" type="text" placeholder="Search.." name="search">
+          <button type="submit"><i class="fa fa-search"></i></button>
+        </form>
+        <div id="items" class="items"></div>
       `;
   },
 
   async afterRender() {
-  }
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurant();
+    const restaurantsContainer = document.querySelector('#items');
+
+    restaurants.forEach((restaurant) => {
+      restaurantsContainer.innerHTML += createRestaurantItemTemplate(restaurant);
+    });
+  },
 };
 
 export default Favorite;
